0d6f0b57132486a4231bc827d045683b8445231e
eduardobaia <edubaia10>
2019-10-26 16:45:39 +0100

    update formule modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+import {FormsModule}  from "@angular/forms";
+
+const appRoutes :Routes = [
+  {
+    path:'notes',
+    component:NotesComponent
+  },
+  {
+    path:'feedback',
+    component:FeedbackComponent
+  },
+  {
+    path:'',
+    component:NotesComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    component: NotfoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FeedbackComponent,
+    NotesComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes,{enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
