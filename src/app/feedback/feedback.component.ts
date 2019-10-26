342d649d1cec55858b448f792b212a391d1e1124
eduardobaia <edubaia10>
2019-10-26 16:58:50 +0100

    creating form to feedback and interface for object java

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackComponent implements OnInit {
 
+
+  model: FeedBackViewModel = {
+    name: '',
+    email:'',
+    feedback:''
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+export interface FeedBackViewModel{
+  name: string;
+  email: string;
+  feedback:string;
 }
